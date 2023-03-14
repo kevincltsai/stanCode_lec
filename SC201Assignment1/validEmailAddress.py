f4b6118ce20f3dab677b4c1b3fe8d27b4996226b
kevincltsai <kevt1981@hotmail.com>
2023-03-14 17:17:52 +0800

    assignment

--- SC201Assignment1/validEmailAddress.py
+++ SC201Assignment1/validEmailAddress.py
@@ -10,6 +10,7 @@ of correct classification.
 
 Accuracy of this model: TODO:
 """
+import os
 
 WEIGHT = [                           # The weight vector selected by Jerry
 	[0.4],                           # (see assignment handout for more details)
@@ -24,13 +25,17 @@ WEIGHT = [                           # The weight vector selected by Jerry
 	[-0.7]
 ]
 
+#DATA_FILE = 'SC201Assignment1/is_valid_email.txt'     # This is the file name to be processed
 DATA_FILE = 'is_valid_email.txt'     # This is the file name to be processed
 
 
 def main():
+	#current_directory()
 	maybe_email_list = read_in_data()
+
 	for maybe_email in maybe_email_list:
 		feature_vector = feature_extractor(maybe_email)
+		print("maybe_email:", maybe_email, " feature_vector :", feature_vector)
 		# TODO:
 
 
@@ -41,11 +46,38 @@ def feature_extractor(maybe_email):
 	"""
 	feature_vector = [0] * len(WEIGHT)
 	for i in range(len(feature_vector)):
-		if i == 0:
+		if i == 0: #'@' in the str
 			feature_vector[i] = 1 if '@' in maybe_email else 0
-		elif i == 1:
+		elif i == 1: #No '.' before '@'
 			if feature_vector[0]:
 				feature_vector[i] = 1 if '.' not in maybe_email.split('@')[0] else 0
+		elif i == 2: #Some strings before '@'
+			if feature_vector[0]:
+   				feature_vector[i] = 1 if bool(maybe_email.split('@')[0]) else 0
+		elif i == 3: #Some strings after '@'
+			if feature_vector[0]:
+   				feature_vector[i] = 1 if bool(maybe_email.split('@')[1]) else 0
+		elif i == 4: #There is '.' after '@'
+			if feature_vector[0]:
+   				feature_vector[i] = 1 if any('.' in x for x in maybe_email.split('@')[1:]) else 0
+		elif i == 5: # There is no white space
+			if feature_vector[0]:
+   				feature_vector[i] = 1 if any(char.isspace() for char in maybe_email) else 0
+		elif i == 6: # Ends with '.com'
+			if feature_vector[0]:
+   				feature_vector[i] = 1 if bool(maybe_email.endswith('.com')) else 0
+		elif i == 7: # Ends with '.edu'
+			if feature_vector[0]:
+   				feature_vector[i] = 1 if bool(maybe_email.endswith('.edu')) else 0
+		elif i == 8: # ends with '.tw'
+			if feature_vector[0]:
+   				feature_vector[i] = 1 if bool(maybe_email.endswith('.tw')) else 0
+		elif i == 9: # Length > 10
+			if feature_vector[0]:
+   				feature_vector[i] = 1 if len(maybe_email) > 10 else 0
+				
+		
+			
 		###################################
 		#                                 #
 		#              TODO:              #
@@ -58,9 +90,12 @@ def read_in_data():
 	"""
 	:return: list, containing strings that might be valid email addresses
 	"""
-	# TODO:
-	pass
-
+	
+	with open(DATA_FILE, "r") as f:
+		lines = f.read().splitlines()
+	return lines
 
 if __name__ == '__main__':
 	main()
+
+
